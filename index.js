6e309341e287f3ac0b2eb6383d6d99945ce7386c
YourWildDad <053700@gmail.com>
2016-03-10 16:43:02 +0800

    修改了一下页面js代码的名字

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ var PORT = 8000;
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
-//var mine = require('./mine').types;
 var path = require('path');
 var server = http.createServer(function(request, response) {
     var pathname = url.parse(request.url).pathname === "/" ? '/index.html' : url.parse(request.url).pathname;
     var realPath = path.join("pages", pathname);
-    //console.log(realPath);
     var ext = path.extname(realPath);
     ext = ext ? ext.slice(1) : 'unknown';
     fs.exists(realPath, function(exists) {
